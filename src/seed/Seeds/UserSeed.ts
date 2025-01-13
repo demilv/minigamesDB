723019942269b64f39423fd28410338ee0e65d37
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-01-13 23:18:01 +0100

    seed User (menos owned)

---
+++ src/seed/Seeds/UserSeed.ts
@@ -0,0 +1,15 @@
+import { UserModel } from '../../Schemas/UserS';
+import { faker } from '@faker-js/faker';
+
+const createRandomUser = () => {
+
+return new UserModel({
+    name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+    email: faker.internet.email(),
+    phone: faker.phone.number().toString(),
+    pass: faker.internet.password(),
+    //owned
+  });
+};
+
+export const randomUsers = Array.from({ length: 10 }, createRandomUser);
