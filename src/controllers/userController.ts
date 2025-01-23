88ac2664c2015d5f23188c40bfc197e7b8530942
demilv <gonzalo.cano@bootcamp-upgrade.com>
2025-01-23 01:03:16 +0100

    userController

---
+++ src/controllers/userController.ts
@@ -0,0 +1,103 @@
+import { Request, Response, NextFunction } from 'express';
+import { User as UserService } from './services/user';
+import { User as UserInterface } from '../interfaces/User';
+import { hashPassword } from '../HashingChecking/HashingChecking/HashCheck';
+import { Types } from 'mongoose';
+
+const fetchAllUsers = async () => {
+    try {
+        const users = await UserService.fetchAll();
+        if (!users) {
+            throw new Error('Users not found');
+        }
+        return users;
+    } catch (e) {
+        throw e;
+    }
+};
+
+
+export const getAllUsers = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await fetchAllUsers();
+        return res.json( users );
+    } catch (e) {
+        return next(e);
+    }
+};
+
+
+export const getOneUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await fetchAllUsers();
+        const userIndex = parseInt(req.params.id, 10)-1;
+
+        if (isNaN(userIndex) || userIndex < 0 || userIndex >= users.length) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const user = users[userIndex];
+        return res.status(200).json(user);
+    } catch (e) {       
+            return next(e);
+        
+    }
+};
+
+export const setNewUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const newUser: UserInterface = req.body;
+        if (!newUser.name || !newUser.email ||
+            !newUser.phone || !newUser.pass || newUser.owned ) 
+        {            
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
+
+        newUser.pass = await hashPassword(newUser.pass);
+
+        const savedUser = await UserService.save(newUser);
+        console.log(savedUser)
+        return res.status(201).json({ user: savedUser });
+    } catch (e) {
+        return next(e);
+    }
+};
+
+export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;  
+        const objectId = new Types.ObjectId(id);
+
+        const updatedUserData: Partial<UserInterface> = req.body;
+        
+        if (updatedUserData.pass) {
+            updatedUserData.pass = await hashPassword(updatedUserData.pass);
+        }
+        const updatedUser = await UserService.Edit(objectId, updatedUserData);
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.json({ user: updatedUser });
+    } catch (e) {
+        return next(e);
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;  
+        const objectId = new Types.ObjectId(id);
+        
+        const deletedUser = await UserService.Delete(objectId);  
+        
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
+        return res.json({ message: 'User deleted successfully' });
+    } catch (e) {
+        return next(e);
+    }
+};
+
